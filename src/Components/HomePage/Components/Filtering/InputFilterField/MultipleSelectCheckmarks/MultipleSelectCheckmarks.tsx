44449e01fb4fd0b4915dc4368844709e67dcdd9a
AbAyvazyan333 <alberthovhannisyan.tt118-1@polytechnic.am>
2023-03-09 17:29:22 +0400

    WIP

--- src/Components/HomePage/Components/Filtering/InputFilterField/MultipleSelectCheckmarks/MultipleSelectCheckmarks.tsx
+++ src/Components/HomePage/Components/Filtering/InputFilterField/MultipleSelectCheckmarks/MultipleSelectCheckmarks.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import "./MultipleSelectCheckmarks.css";
-import {ChangeEvent, FC, useEffect, useRef, useState} from "react";
+import { ChangeEvent, FC, useEffect, useRef, useState } from "react";
 
 export type Tmodels = {
   title: string;
-  isChecked:boolean;
-  _id:string;
+  isChecked: boolean;
+  _id: string;
 };
 
 interface TSelect {
@@ -17,6 +17,8 @@ interface TSelect {
   isOpen: boolean;
 }
 
+type Tfrom_to = { from: number | undefined; to: number | undefined };
+
 const MultipleSelectCheckmarks: FC<TSelect> = ({
   _id,
   holder,
@@ -27,9 +29,13 @@ const MultipleSelectCheckmarks: FC<TSelect> = ({
 }) => {
   const [checkboxesModal, setCheckboxesModal] = useState<Tmodels[]>();
   const [Chosen, setChosen] = useState<string[]>([]);
+  const [from_to_obj, set_from_to_obj] = useState<Tfrom_to>({
+    from: undefined,
+    to: undefined,
+  });
 
   useEffect(() => {
-      setCheckboxesModal(models);
+    setCheckboxesModal(models);
   }, []);
 
   const modalRef = useRef<HTMLDivElement>(null);
@@ -40,28 +46,35 @@ const MultipleSelectCheckmarks: FC<TSelect> = ({
         openModalHandler(undefined);
       }
     };
-    window.addEventListener("click", listener);
+    isOpen && window.addEventListener("click", listener);
+
     return () => {
       window.removeEventListener("click", listener);
     };
-  }, []);
+  }, [isOpen]);
 
-  const handleCheckbox = ({target}:ChangeEvent<HTMLInputElement>,_id:string,isChecked:boolean)=>{
-      setCheckboxesModal((p)=>{
-        return p && p.map((v)=>{
-          if(v._id === _id){
-            v.isChecked = !v.isChecked
+  const handleCheckbox = (
+    { target }: ChangeEvent<HTMLInputElement>,
+    _id: string,
+    isChecked: boolean
+  ): void => {
+    setCheckboxesModal((p) => {
+      return (
+        p &&
+        p.map((v) => {
+          if (v._id === _id) {
+            v.isChecked = !v.isChecked;
           }
-          return v
+          return v;
         })
-      })
-       setChosen((p)=>{
-         if(isChecked){
-           return [...p.filter(v=>v!== target.value)]
-         }
-         return [...p,target.value]
-       })
-  }
+      );
+    });
+    setChosen((p) =>
+      isChecked
+        ? [...p.filter((v) => v !== target.value)]
+        : [...p, target.value]
+    );
+  };
 
   return (
     <>
@@ -74,7 +87,17 @@ const MultipleSelectCheckmarks: FC<TSelect> = ({
         ref={modalRef}
       >
         <>
-          {!Chosen.length ?  <span>{holder}</span>  : Chosen.map((v,i) => <span key={i}>{v}</span>)}
+          {!Chosen.length ? (
+              <>
+                {from_to_obj.from ? <span>from:{from_to_obj.from}</span> : <span>{holder}</span>}
+                {from_to_obj.to && <span>to:{from_to_obj.to}</span>}
+              </>
+          ) : Chosen.length >= 3 ? (
+            [Chosen[0], Chosen[1]].map((v, i) => <span key={i}>{v}</span>)
+          ) : (
+            Chosen.map((v, i) => <span key={i}>{v}</span>)
+          )}
+          {Chosen.length >= 3 && <span>...</span>}
           <span className={isOpen ? "select_arrow_clicked" : "select_arrow"}>
             <i className="fa fa-angle-down"></i>
           </span>
@@ -89,11 +112,17 @@ const MultipleSelectCheckmarks: FC<TSelect> = ({
           >
             {!fromTo &&
               checkboxesModal &&
-              checkboxesModal.map(({title,_id,isChecked}, index) => {
+              checkboxesModal.map(({ title, _id, isChecked }, index) => {
                 return (
                   <div key={_id} className={"checkobox_modal_selects"}>
                     <label className="form-control">
-                      <input type="checkbox" name="checkbox" checked={isChecked} value={title} onChange={(e)=>handleCheckbox(e,_id,isChecked)}/>
+                      <input
+                        type="checkbox"
+                        name="checkbox"
+                        checked={isChecked}
+                        value={title}
+                        onChange={(e) => handleCheckbox(e, _id, isChecked)}
+                      />
                       {title}
                     </label>
                   </div>
@@ -102,8 +131,27 @@ const MultipleSelectCheckmarks: FC<TSelect> = ({
 
             {fromTo && (
               <div className={"from_to_modal"}>
-                <input type="number" placeholder={"from"} />
-                <input type="number" placeholder={"to"} />
+                <input
+                  type="number"
+                  placeholder={"from"}
+                  value={from_to_obj.from}
+                  onChange={(e) =>
+                    set_from_to_obj((p) => {
+                      return { to: from_to_obj.to, from: +e.target.value };
+                    })
+                  }
+                />
+                <input
+                  type="number"
+                  placeholder={"to"}
+                  value={ from_to_obj.from && from_to_obj.to && from_to_obj.from > from_to_obj.to ? from_to_obj.from : from_to_obj.to}
+                  onChange={(e) =>
+                    set_from_to_obj((p) => {
+
+                      return { to: +e.target.value, from: from_to_obj.from };
+                    })
+                  }
+                />
               </div>
             )}
           </div>
