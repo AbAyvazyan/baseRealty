c1767cab63ccce25b8f74aac71316d7f91c756e7
AbAyvazyan333 <abayvazyan333@gmail.com>
2023-04-26 17:14:37 +0400

    working on house posting UI

--- src/Components/HousePosting/HousePosting.tsx
+++ src/Components/HousePosting/HousePosting.tsx
@@ -5,7 +5,7 @@ const HousePosting = () =>{
        <section className={'house_posting'}>
 
            <div>
-               <input type="text" placeholder={'username'}/>
+               <input type="text" placeholder={'Կոդ'}/>
            </div>
 
 
@@ -114,7 +114,18 @@ const HousePosting = () =>{
                                         <div >
 
                                             <select>
-                                                <option value="Arabkir">Արաբկիր</option>
+                                                <option value="arabkir">Արաբկիր</option>
+                                                <option value="kentron">Կենտրոն</option>
+                                                <option value="davtashen">Դավթաշեն</option>
+                                                <option value="ajapnyak">Աջափնյակ</option>
+                                                <option value="avan">Ավան</option>
+                                                <option value="erebuni">Էրեբունի</option>
+                                                <option value="qanaqer">Քանաքեռ-Զեյթուն</option>
+                                                <option value="malatia">Մալաթիա-Սեբաստիա</option>
+                                                <option value="norq-marash">Նորք-Մարաշ</option>
+                                                <option value="nor-norq">Նոր Նորք</option>
+                                                <option value="nubarashen">Նուբարաշեն</option>
+                                                <option value="shengavit">Շենգավիթ</option>
                                             </select>
                                         </div>
                                     </div>
@@ -166,11 +177,17 @@ const HousePosting = () =>{
                        <div >
 
                            <select>
-                               <option value="Arabkir">Մոնոլիտ</option>
-                               <option value="Arabkir">Քարե</option>
-                               <option value="Arabkir">Պանելային</option>
-                               <option value="Arabkir">Նորակառույց</option>
-                               <option value="Arabkir">Այլ</option>
+                               <option value="qare">Ստալինկա</option>
+                               <option value="qare">Քարե</option>
+                               <option value="qare">Պոստ Խրյուշովկա</option>
+                               <option value="qare">Խրյուշովկա</option>
+                               <option value="panelain">Բարձրահարկ</option>
+                               <option value="panelain">Խոշոր Պանելային</option>
+                               <option value="panelain">Կասետաին</option>
+                               <option value="monolith">Նորակառույց</option>
+                               <option value="monolith">Նորակառույց Կառուցապատողից</option>
+                               <option value="ayl">Այլ</option>
+                               {/*monolity norakaruycna*/}
                            </select>
                        </div>
                    </div>
@@ -249,11 +266,14 @@ const HousePosting = () =>{
 
 
                                     <select>
-                                        <option value="2.45">2.45</option>
-                                        <option value="2.55">2.55</option>
-                                        <option value="2.75<">2.75</option>
-                                        <option value="3">3</option>
-                                        <option value="3.20">3.20</option>
+                                        <option value="old">հին վերանորոգում</option>
+                                        <option value="zero-cond">զրոական</option>
+                                        <option value="old<">վերանորոգում 10 ավել</option>
+                                        <option value="old">վերանորոգում 5-10</option>
+                                        <option value="good">վերանորոգում 2-5</option>
+                                        <option value="good">վերանորոգված 2 տարի</option>
+                                        <option value="new">վերանորոգված չբնակեցված</option>
+                                        <option value="new">վերանորոգված դիզաիներական ոճով</option>
                                     </select>
 
                                 </div>
@@ -266,40 +286,57 @@ const HousePosting = () =>{
 
            </div>
 
+           <div className={'owner_contact_posting'}>
+                <h2>Կոնտակտներ</h2>
+
+                <div>
+                    <div className={'contact_input_posting'}>
+                        <input type="text" placeholder={'Անուն'}/>
+                        <input type="text" placeholder={'Հեռախոսահամար'}/>
+                        <input type="text" placeholder={'Մաիլ'}/>
+                    </div>
+
+
+                </div>
+
+
+               <div><div className={'add_contact_section'}>+</div></div>
+           </div>
+
 
            <div className={'information_posting'}>
 
            <div className={'div_checkboxes'}>
                 <div>ԿՈՄՈՒՆԱԼ</div>
-                <div>
+                <div style={{gridTemplateColumns:'1fr 1fr'}}>
                     <label>
                         <input type="checkbox"/>
-                        Ջեռուցում
+                        Գազ
                     </label>
 
                     <label>
                         <input type="checkbox"/>
-                        Ջեռուցում
+                        էլեկտրոէներգիա
                     </label>
 
                     <label>
                         <input type="checkbox"/>
-                        Ջեռուցում
+                        Ջուր
                     </label>
 
                     <label>
                         <input type="checkbox"/>
-                        Ջեռուցում
+                        Համացանց
                     </label>
 
                     <label>
                         <input type="checkbox"/>
-                        Ջեռուցում
+                        Ջեռուցման համակարգ
                     </label>
 
                     <label>
                         <input type="checkbox"/>
-                        Ջեռուցում
+                        Օդորակիչ
                     </label>
                 </div>
            </div>
@@ -309,32 +346,112 @@ const HousePosting = () =>{
                    <div>
                        <label>
                            <input type="checkbox"/>
-                           Ջեռուցում
+                           Արևկողմ
                        </label>
 
                        <label>
                            <input type="checkbox"/>
-                           Ջեռուցում
+                           Ավտոկայանատեղի
                        </label>
 
                        <label>
                            <input type="checkbox"/>
-                           Ջեռուցում
+                           Անվտանգություն
                        </label>
 
                        <label>
                            <input type="checkbox"/>
-                           Ջեռուցում
+                           Բուխարրի
                        </label>
 
                        <label>
                            <input type="checkbox"/>
-                           Ջեռուցում
+                           Բաց պատշգամբ
                        </label>
 
                        <label>
                            <input type="checkbox"/>
-                           Ջեռուցում
+                           Լոջա
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Վերելակ
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Նկուղ
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Ձեղնահարկ
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Խորդանոց
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Բարձր առաջին հարկ
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Տաքացվող հատակ
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Գեղեցիկ տեսարան
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Կանգառի մոտ
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Մետրո
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Ճանապարհամերձ
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Զբոսայգի
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Պարսպապատ
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Շոգեբաղնիք
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Կահույք
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Տեխնիկա
+                       </label>
+
+                       <label>
+                           <input type="checkbox"/>
+                           Խոհանոցաին Կահույք
                        </label>
                    </div>
                </div>
@@ -348,7 +465,7 @@ const HousePosting = () =>{
                     <div className={'file_input_part'}>
                         <label className={'file_input_label'}>
                             <input type="file" multiple={true} hidden/>
-                            <span>Լուսանկար</span>
+                            <span>Լուսանկար մեր համար</span>
                         </label>
                     </div>
 
@@ -361,7 +478,7 @@ const HousePosting = () =>{
                    <div className={'file_input_part'}>
                        <label className={'file_input_label'}>
                            <input type="file" multiple={true} hidden/>
-                           <span>Լուսանկար</span>
+                           <span>Լուսանկար Ընդհաննուր</span>
                        </label>
                    </div>
 
@@ -374,7 +491,7 @@ const HousePosting = () =>{
                    <div className={'file_input_part'}>
                        <label className={'file_input_label'}>
                            <input type="file" multiple={true} hidden/>
-                           <span>Լուսանկար</span>
+                           <span>Լուսանկար Վկայականի</span>
                        </label>
                    </div>
 
