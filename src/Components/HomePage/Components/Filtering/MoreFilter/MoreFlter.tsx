da9c6558ccb75f305f67d1a757bf9ec975b7c8b3
AbAyvazyan333 <abayvazyan333@gmail.com>
2023-08-19 12:39:59 +0400

    added onOutSide click one more fiilter modal

--- src/Components/HomePage/Components/Filtering/MoreFilter/MoreFlter.tsx
+++ src/Components/HomePage/Components/Filtering/MoreFilter/MoreFlter.tsx
@@ -1,47 +1,50 @@
 import './MoreFilter.css'
-
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faXmark} from '@fortawesome/free-solid-svg-icons'
-import {FC} from "react";
-import * as React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FC, useRef, useEffect } from "react"; // Import useRef and useEffect
 import MoreFilterRangePart from "./MoreFilterRangePart/MoreFilterRangePart";
 import MoreFilterButtonGroup from "./MoreFilterButtonGroup/MoreFilterButtonGroup";
 import SingleCheck from "./SingleCheck/SingleCheck";
 import MultipleChecks from "./MultipleChecks/MultipleChecks";
-import {useTranslation} from "react-i18next";
-
+import { useTranslation } from "react-i18next";
 
 type TMoreFilter = {
-    closeModalHandler:()=>void
+    closeModalHandler: () => void
 }
 
-const MoreFilter:FC<TMoreFilter> = ({closeModalHandler}) =>{
+const MoreFilter: FC<TMoreFilter> = ({ closeModalHandler }) => {
+    const { t } = useTranslation();
 
-    const {t} = useTranslation()
-    return(
-        <div className={'more_filter'}>
-                <div className={'more_filter_upper_part'}>
-                    <span>{t('More_Filters')}</span>
-                    <span style={{cursor:'pointer'}} onClick={()=>closeModalHandler()}><FontAwesomeIcon icon={faXmark} /></span>
-                </div>
+    const modalRef = useRef<any>(null); // Create a ref for the modal
 
-                <div className={'more_filter_content'}>
+    const handleOutsideClick = (event:MouseEvent) => {
+        if (modalRef.current && !modalRef.current.contains(event.target)) {
+            closeModalHandler();
+        }
+    };
 
-                        <MoreFilterRangePart name={'Floor'}/>
-                        <MoreFilterRangePart name={'Building_Floors'}/>
+    useEffect(() => {
+        document.addEventListener('mousedown', handleOutsideClick);
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, []);
 
-                        <MoreFilterButtonGroup/>
-
-
-                        <MultipleChecks title={'House_Type'} houseTypes={['Stone','Monolith','Panel','Other']}/>
-                        <MultipleChecks title={'Renovation'} houseTypes={['Zero_Condition','Newly_repaired','Good','Old_Renovation']}/>
-
-
-                    </div>
-                </div>
-    )
+    return (
+        <div className={'more_filter'} ref={modalRef}>
+            <div className={'more_filter_upper_part'}>
+                <span>{t('More_Filters')}</span>
+                <span style={{ cursor: 'pointer' }} onClick={closeModalHandler}><FontAwesomeIcon icon={faXmark} /></span>
+            </div>
+            <div className={'more_filter_content'}>
+                <MoreFilterRangePart name={'Floor'} />
+                <MoreFilterRangePart name={'Building_Floors'} />
+                <MoreFilterButtonGroup />
+                <MultipleChecks title={'House_Type'} houseTypes={['Stone', 'Monolith', 'Panel', 'Other']} />
+                <MultipleChecks title={'Renovation'} houseTypes={['Zero_Condition', 'Newly_repaired', 'Good', 'Old_Renovation']} />
+            </div>
+        </div>
+    );
 }
 
-
-export default MoreFilter
+export default MoreFilter;
